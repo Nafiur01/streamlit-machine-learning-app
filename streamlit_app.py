b36f36e13ae75dd64d71f80b64495fcc1f50403d
Nafiur Rahman <36277716+Nafiur01@users.noreply.github.com>
2025-09-22 22:47:09 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ with st.sidebar:
   input_df = pd.DataFrame(data,index=[0])
   input_penguins = pd.concat([input_df,X], axis=0)
 
-with st.expander('Input Features')
+with st.expander('Input Features'):
   st.write('**Input**')
   input_df
   st.write('**Combined Input Features**')
