2627375193ddca9a22f25eca88ef183913b258de
Nafiur Rahman <36277716+Nafiur01@users.noreply.github.com>
2025-09-22 15:35:42 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 st.title('Penguin Species Prediction with Machine Learning')
 
 st.info('We are going to predict penguin species based on the dataset using machine learning')
+
+with st.expander('Data'):
+  st.write('**Raw Data**')
